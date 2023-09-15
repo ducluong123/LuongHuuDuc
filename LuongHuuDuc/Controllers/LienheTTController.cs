0ba1be8d8b4acb7fd4cf937be62c91a87bdc8079
ducluong123 <lhduc.lm2001@gmail.com>
2023-09-15 22:14:39 +0700

    Add project files.

---
+++ LuongHuuDuc/Controllers/LienheTTController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LuongHuuDuc.Controllers
+{
+    public class LienheTTController : Controller
+    {
+        public IActionResult Thongtinhocvien()
+        {
+            ViewData["Hovaten"] = "Lương Hữu Đức";
+            ViewData["Ngaysinh"] = "25/01/2001";
+            ViewData["Email"] = "lhduc.lm2001@gmail.com";
+            ViewData["Sodienthoai"] = "0563250798";
+            return View();
+        }
+    }
+}
